f2d60db6506c84fb23befcd172b1e9d8472267e0
Karim El-Akhras <kme29@mail.aub.edu>
2023-04-11 10:00:16 +0200

    Added dictionary for cities

---
+++ public/city_dictionary.js
@@ -0,0 +1,10 @@
+var citydictionary = {
+    "Beirut, Lebanon": "Beirut, Lebanon",
+    "Sidon, Lebanon": "Sidon, Lebanon",
+    "Tripoli, Lebanon": "Tripoli, Lebanon",
+    "Jbeil, Lebanon": "Jbeil, Lebanon",
+    "Bekaa, Lebanon": "Bekaa, Lebanon",
+    "Tir, Lebanon": "Tir, Lebanon",
+    "Jounieh, Lebanon": "Jounieh, Lebanon",
+    "Baalbek, Lebanon": "Baalbek, Lebanon",
+  };
