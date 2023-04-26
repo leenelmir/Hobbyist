f594ff17cd6f2824555190125f270c4574f45f77
leenelmir <leenelmirr@gmail.com>
2023-04-26 13:14:18 +0300

    added more routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -3,12 +3,26 @@ const authenticateUser = require("../middleware/auth");
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 router.use(cookieParser());
+const { Profile } = require("../models/profile");
 
-router.get("/", authenticateUser, (req, res) => {
-    return res.status(200).render("rooms.ejs");
+router.get("/", authenticateUser, async (req, res) => {
+
+    try {
+        const userId = req.user._id;
+        const profile = await Profile.findOne({ user: userId }).populate('user');
+        if(!profile)
+        {
+            return res.status(404).json({status: "Profile was not found!"});
+        }
+  
+       return res.status(200).render('rooms.ejs', {myProfile : profile});
+    }
+    catch (error) {
+        return res.status(500).json({ error: 'Server error' });
+    }
 });
 
-router.get("/:room", authenticateUser, async (req, res) => {
+router.get("/:room", authenticateUser, async (req, res) => { 
     try
     {
         const { room } = req.params;
