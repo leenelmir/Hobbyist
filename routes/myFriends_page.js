8d0bc7b16d93927f481facf1b4ecd1a9ed799716
leenelmir <leenelmirr@gmail.com>
2023-04-27 20:15:09 +0300

    trial change

--- routes/myFriends_page.js
+++ routes/myFriends_page.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 router.use(cookieParser());
 const authenticateUser = require("../middleware/auth");
 const { Profile } = require("../models/profile");
+const { Friendship } = require("../models/friendship");
+const { User } = require("../models/user");
+
 
 router.get('/', authenticateUser, async (req, res) =>{
 
@@ -15,6 +18,26 @@ router.get('/', authenticateUser, async (req, res) =>{
             console.error(err);
             res.status(500).send({status: "Profile not found"});
         }
+});
+
+router.post("/check-request", authenticateUser, async (req, res) => {     
+    
+    console.log("check request: " + req.body.myUserId);
+
+    try
+    {
+        const user = await User.findOne({user: req.user._id});
+        const friendships = await Friendship.findOne({receiver: req.user._id});
+        if (!friendships){
+            return res.status(200).json({status: "No requests!"});
+        }
+        return res.status(200).json({status: "done", friendships : friendships});
+    }
+    catch(err)
+    {
+        console.error(err);
+        return res.status(500).json({status : "Server error"});
+    }
 });
 
 module.exports = router;
